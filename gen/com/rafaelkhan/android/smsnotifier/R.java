177749a2684351e5c0b53eb9cbcad68e55af2b62
Rafael Khan <raf.khan.f@gmail.com>
2011-08-09 02:12:33 -0400

    Updated UI

--- gen/com/rafaelkhan/android/smsnotifier/R.java
+++ gen/com/rafaelkhan/android/smsnotifier/R.java
@@ -11,14 +11,20 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int gradient_divider=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
         public static final int contact_name_view=0x7f060002;
+        public static final int dismiss_button=0x7f060008;
         public static final int imageView1=0x7f060001;
+        public static final int linearLayout1=0x7f060007;
         public static final int message_body_view=0x7f060004;
         public static final int phone_number_view=0x7f060003;
         public static final int relativeLayout1=0x7f060000;
+        public static final int reply_button=0x7f060009;
+        public static final int send_button=0x7f060005;
+        public static final int text_input=0x7f060006;
     }
     public static final class layout {
         public static final int receiver_dialog_layout=0x7f030000;
@@ -27,6 +33,7 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int dismiss=0x7f040001;
         public static final int reply=0x7f040002;
+        public static final int send=0x7f040004;
         public static final int white_hex=0x7f040003;
     }
     public static final class style {
--- src/com/rafaelkhan/android/smsnotifier/SMSReceivedActivity.java
+++ src/com/rafaelkhan/android/smsnotifier/SMSReceivedActivity.java
@@ -1,17 +1,31 @@
 package com.rafaelkhan.android.smsnotifier;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import android.app.Activity;
+import android.content.ContentUris;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract.Contacts;
 import android.provider.ContactsContract.PhoneLookup;
+import android.provider.ContactsContract.CommonDataKinds.Photo;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class SMSReceivedActivity extends Activity {
 
 	private String sender; // senders phone number
+	private String name; // senders name
 	private String message; // SMS text body
+	private long id;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -28,6 +42,7 @@ public class SMSReceivedActivity extends Activity {
 		if (bundle != null) {
 			this.sender = bundle.getString("sender");
 			this.message = bundle.getString("message");
+			this.name = getContactName(this.sender);
 			return true;
 		} else {
 			return false;
@@ -38,22 +53,68 @@ public class SMSReceivedActivity extends Activity {
 		TextView contactNameView = (TextView) findViewById(R.id.contact_name_view);
 		TextView phoneNumberView = (TextView) findViewById(R.id.phone_number_view);
 		TextView messageBodyView = (TextView) findViewById(R.id.message_body_view);
-		@SuppressWarnings("unused")
 		ImageView contactImageView = (ImageView) findViewById(R.id.imageView1);
 
-		contactNameView.setText(getContactName(this.sender));
+		contactNameView.setText(this.name);
 		phoneNumberView.setText(this.sender);
 		messageBodyView.setText(this.message);
+
+		InputStream is = this.openPhoto(this.id);
+		if (is != null) {
+			Bitmap bm = BitmapFactory.decodeStream(is);
+			contactImageView.setImageBitmap(bm);
+		}
 	}
 
-	private String getContactName(String number) {	
-		Uri uri = Uri.withAppendedPath(PhoneLookup.CONTENT_FILTER_URI, Uri.encode(number)); 
-		Cursor c = getApplicationContext().getContentResolver().query(uri, new String[]{PhoneLookup.DISPLAY_NAME}, null, null, null);  
-		if(c.moveToNext()){   
-			String name = c.getString(c.getColumnIndexOrThrow(PhoneLookup.DISPLAY_NAME));
+	private String getContactName(String number) { // this method also sets the
+													// iD
+		Uri uri = Uri.withAppendedPath(PhoneLookup.CONTENT_FILTER_URI, Uri
+				.encode(number));
+		Cursor c = getApplicationContext().getContentResolver().query(uri,
+				new String[] { PhoneLookup.DISPLAY_NAME, PhoneLookup._ID },
+				null, null, null);
+		if (c.moveToNext()) {
+			String name = c.getString(c
+					.getColumnIndexOrThrow(PhoneLookup.DISPLAY_NAME));
+			this.id = c.getColumnIndexOrThrow(PhoneLookup._ID);
+			Log.d("asd", Long.toString(this.id));
 			return name;
 		} else {
 			return "Unknown";
 		}
+	}
+
+	public InputStream openPhoto(long contactId) {
+		Uri contactUri = ContentUris.withAppendedId(Contacts.CONTENT_URI,
+				contactId);
+		Uri photoUri = Uri.withAppendedPath(contactUri,
+				Contacts.Photo.CONTENT_DIRECTORY);
+		Cursor cursor = getContentResolver().query(photoUri,
+				new String[] { Photo.PHOTO }, null, null, null);
+		if (cursor == null) {
+			return null;
+		}
+		try {
+			if (cursor.moveToFirst()) {
+				byte[] data = cursor.getBlob(0);
+				if (data != null) {
+					return new ByteArrayInputStream(data);
+				}
+			}
+		} finally {
+			cursor.close();
+		}
+		return null;
+	}
+	
+	public void replyButton(View v) {
+		EditText et = (EditText) findViewById(R.id.text_input);
+		Button bt = (Button) findViewById(R.id.send_button);
+		et.setVisibility(View.VISIBLE);
+		bt.setVisibility(View.VISIBLE);
+	}
+	
+	public void dismissButton(View v) {
+		moveTaskToBack(true);
 	}
 }
